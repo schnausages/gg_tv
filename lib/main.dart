bc768d71a26cddfe80308a361d4f3bb5cc1f939e
drew <drew@scrapprecycling.com>
2023-07-31 20:17:35 -0500

    buttons, font, main feed ui

--- lib/main.dart
+++ lib/main.dart
@@ -77,55 +77,90 @@ class _HomePageState extends State<HomePage> {
     const Color _activeColor = Color(0xFFE23FFF);
     const Color _inactiveColor = Color.fromARGB(255, 72, 65, 77);
     return Scaffold(
-      body: PageView(
-        controller: _pageController,
-        physics: NeverScrollableScrollPhysics(),
-        children: _screens,
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-          backgroundColor: Colors.transparent,
-          type: BottomNavigationBarType.fixed,
-          elevation: 0,
-          showSelectedLabels: false,
-          selectedItemColor: Color(0xFFE23FFF),
-          unselectedItemColor: Colors.blueGrey,
-          currentIndex: _pageIndex,
-          onTap: _onItemTapped,
-          items: [
-            BottomNavigationBarItem(
-                icon: Icon(Icons.play_arrow_rounded), label: ''),
-            BottomNavigationBarItem(icon: Icon(Icons.add), label: ''),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.leaderboard_rounded), label: ''),
-            BottomNavigationBarItem(icon: Icon(Icons.person_2), label: ''),
-          ]),
-      // bottomNavigationBar: Container(
-      //   height: _h * .075,
-      //   width: _w,
-      //   color: Colors.black,
-      //   child: Row(children: [
-      //     GestureDetector(
-      //       onTap: () => _onItemTapped(0),
-      //       child: Icon(Icons.play_arrow_rounded,
-      //           color: _pageIndex == 0 ? _activeColor : _inactiveColor),
-      //     ),
-      //     GestureDetector(
-      //       onTap: () => _onItemTapped(1),
-      //       child: Icon(Icons.add,
-      //           color: _pageIndex == 1 ? _activeColor : _inactiveColor),
-      //     ),
-      //     GestureDetector(
-      //       onTap: () => _onItemTapped(2),
-      //       child: Icon(Icons.leaderboard_rounded,
-      //           color: _pageIndex == 2 ? _activeColor : _inactiveColor),
-      //     ),
-      //     GestureDetector(
-      //       onTap: () => _onItemTapped(3),
-      //       child: Icon(Icons.person_2,
-      //           color: _pageIndex == 3 ? _activeColor : _inactiveColor),
-      //     )
-      //   ]),
-      // )
-    );
+        body: Stack(
+      alignment: Alignment.bottomCenter,
+      children: [
+        PageView(
+          controller: _pageController,
+          physics: NeverScrollableScrollPhysics(),
+          children: _screens,
+        ),
+        Container(
+          width: _w,
+          height: 45,
+          color: _pageIndex == 0 ? Colors.transparent : Colors.blueGrey,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              GestureDetector(
+                onTap: () => _onItemTapped(0),
+                child: Icon(Icons.play_arrow_rounded,
+                    color: _pageIndex == 0 ? _activeColor : _inactiveColor),
+              ),
+              GestureDetector(
+                onTap: () => _onItemTapped(1),
+                child: Icon(Icons.add,
+                    color: _pageIndex == 1 ? _activeColor : _inactiveColor),
+              ),
+              GestureDetector(
+                onTap: () => _onItemTapped(2),
+                child: Icon(Icons.leaderboard_rounded,
+                    color: _pageIndex == 2 ? _activeColor : _inactiveColor),
+              ),
+              GestureDetector(
+                onTap: () => _onItemTapped(3),
+                child: Icon(Icons.person_2,
+                    color: _pageIndex == 3 ? _activeColor : _inactiveColor),
+              )
+            ],
+          ),
+        )
+      ],
+    )
+        // bottomNavigationBar: BottomNavigationBar(
+        //     backgroundColor: Colors.transparent,
+        //     type: BottomNavigationBarType.fixed,
+        //     elevation: 0,
+        //     showSelectedLabels: false,
+        //     selectedItemColor: Color(0xFFE23FFF),
+        //     unselectedItemColor: Colors.blueGrey,
+        //     currentIndex: _pageIndex,
+        //     onTap: _onItemTapped,
+        //     items: [
+        //       BottomNavigationBarItem(
+        //           icon: Icon(Icons.play_arrow_rounded), label: ''),
+        //       BottomNavigationBarItem(icon: Icon(Icons.add), label: ''),
+        //       BottomNavigationBarItem(
+        //           icon: Icon(Icons.leaderboard_rounded), label: ''),
+        //       BottomNavigationBarItem(icon: Icon(Icons.person_2), label: ''),
+        //     ]),
+        // bottomNavigationBar: Container(
+        //   height: _h * .075,
+        //   width: _w,
+        //   color: Colors.black,
+        //   child: Row(children: [
+        //     GestureDetector(
+        //       onTap: () => _onItemTapped(0),
+        //       child: Icon(Icons.play_arrow_rounded,
+        //           color: _pageIndex == 0 ? _activeColor : _inactiveColor),
+        //     ),
+        //     GestureDetector(
+        //       onTap: () => _onItemTapped(1),
+        //       child: Icon(Icons.add,
+        //           color: _pageIndex == 1 ? _activeColor : _inactiveColor),
+        //     ),
+        //     GestureDetector(
+        //       onTap: () => _onItemTapped(2),
+        //       child: Icon(Icons.leaderboard_rounded,
+        //           color: _pageIndex == 2 ? _activeColor : _inactiveColor),
+        //     ),
+        //     GestureDetector(
+        //       onTap: () => _onItemTapped(3),
+        //       child: Icon(Icons.person_2,
+        //           color: _pageIndex == 3 ? _activeColor : _inactiveColor),
+        //     )
+        //   ]),
+        // )
+        );
   }
 }
--- lib/screens/main_feed.dart
+++ lib/screens/main_feed.dart
@@ -1,3 +1,4 @@
+import 'package:animated_react_button/animated_react_button.dart';
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 
@@ -10,6 +11,7 @@ class MainFeedScreen extends StatefulWidget {
 
 class _MainFeedScreenState extends State<MainFeedScreen> {
   late VideoPlayerController _controller;
+  final SizedBox _box = const SizedBox(height: 10);
 
   @override
   void initState() {
@@ -31,14 +33,69 @@ class _MainFeedScreenState extends State<MainFeedScreen> {
                 itemCount: 12,
                 scrollDirection: Axis.vertical,
                 itemBuilder: (context, i) {
-                  return SizedBox.expand(
-                    child: FittedBox(
-                      fit: BoxFit.cover,
-                      child: SizedBox(
-                          width: _controller.value.size.width,
-                          height: _controller.value.size.height,
-                          child: VideoPlayer(_controller)),
-                    ),
+                  return Stack(
+                    children: [
+                      SizedBox.expand(
+                        child: FittedBox(
+                          fit: BoxFit.cover,
+                          child: SizedBox(
+                              width: _controller.value.size.width,
+                              height: _controller.value.size.height,
+                              child: VideoPlayer(_controller)),
+                        ),
+                      ),
+                      const Positioned(
+                          bottom: 80,
+                          child: Text(
+                            'aceTAY_2 on Valorant',
+                            style: TextStyle(
+                                fontFamily: 'Giga',
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18),
+                          )),
+                      Positioned(
+                          right: 10,
+                          top: 40,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              AnimatedReactButton(
+                                  defaultColor: Colors.blueGrey,
+                                  reactColor: Color.fromRGBO(255, 202, 44, 1),
+                                  iconSize: 42,
+                                  defaultIcon: Icons.star_rounded,
+                                  showSplash: true,
+                                  onPressed: () {
+                                    //like
+                                  }),
+                              _box,
+                              AnimatedReactButton(
+                                  defaultColor: Colors.blueGrey,
+                                  reactColor: Color.fromARGB(255, 91, 255, 42),
+                                  iconSize: 54,
+                                  defaultIcon: Icons.keyboard_arrow_up_rounded,
+                                  showSplash: true,
+                                  onPressed: () {
+                                    //like
+                                  }),
+                              _box,
+                              AnimatedReactButton(
+                                  defaultColor: Colors.blueGrey,
+                                  reactColor: Color.fromARGB(255, 255, 65, 61),
+                                  iconSize: 54,
+                                  defaultIcon:
+                                      Icons.keyboard_arrow_down_rounded,
+                                  showSplash: true,
+                                  onPressed: () {
+                                    //like
+                                  }),
+                              _box,
+                              Icon(Icons.chat_bubble_outline_rounded,
+                                  color: Colors.white)
+                            ],
+                          ))
+                    ],
                   );
                 })
             : const CircularProgressIndicator(
